a4a47ec88a1b7f3ca709c487bdefea78e19087b3
Danny Neel <dneel10@gmail.com>
2022-11-11 22:54:23 -0500

    create board class and knight class.  Board class can update place knight shows on board

---
+++ board.rb
@@ -0,0 +1,40 @@
+# frozen_string_literal: true
+require_relative './display'
+require_relative './knight'
+
+class Board
+  include Display
+
+  attr_accessor :board, :knight
+
+  def initialize
+    @board = create_board
+    @knight = Knight.new
+  end
+
+  def create_board
+    board = Array.new(8) {Array.new(8, ' ')}
+    return board
+  end
+
+  def place_knight(position = [0, 1], knight = @knight)
+    self.board[position[0]][position[1]] = 'N'
+    knight.position = position
+    return knight
+  end
+
+  def knight_move(final_position, position = knight.position)
+    board[position[0]][position[1]] = ' '
+    board[final_position[0]][final_position[1]] = 'N'
+  end
+end
+
+gameboard = Board.new
+puts "#{gameboard.display_board}"
+gameboard.place_knight([4,3])
+puts "#{gameboard.display_board}"
+
+gameboard.knight_move([4,1])
+puts "#{gameboard.display_board}"
+
+
---
+++ knight.rb
@@ -0,0 +1,16 @@
+# frozen_string_literal: true
+
+class Knight
+  attr_accessor :position
+  
+  def initialize(position = [0,1])
+    @position = position
+
+    @possible_moves = []
+    
+  end
+
+  def knight_move(current_position, final_position)
+
+  end
+end
