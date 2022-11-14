b4bea2812673973c91cc0d84617c4b9a724b614c
Danny Neel <dneel10@gmail.com>
2022-11-14 18:34:57 -0500

    add main functionality to bfs and Knight Class.  build_path output still broken

--- board.rb
+++ board.rb
@@ -1,40 +1,45 @@
 # frozen_string_literal: true
-require_relative './display'
 require_relative './knight'
 
 class Board
-  include Display
-
-  attr_accessor :board, :knight
-
   def initialize
-    @board = create_board
     @knight = Knight.new
   end
 
-  def create_board
-    board = Array.new(8) {Array.new(8, ' ')}
-    return board
+  def bfs(pos = @knight.pos, target)
+    queue = [Knight.new(target)]
+    visited = []
+    while queue.any?
+      current_pos = queue.shift
+
+      return build_path(pos, target, visited) if current_pos.pos == pos
+      visited << current_pos
+
+
+      moves = possible_moves(current_pos.pos)
+      moves.each do |move|
+        queue << knight = Knight.new(move, current_pos) unless queue.include?(knight) || visited.include?(knight)
+      end
+    end
+    visited    
   end
 
-  def place_knight(position = [0, 1], knight = @knight)
-    self.board[position[0]][position[1]] = 'N'
-    knight.position = position
-    return knight
+  def possible_moves(pos = @knight.pos, moves = @knight.moves)
+    moves.map {|x, y| [pos[0] + x, pos[1] + y] }.
+    select {|move| move.all? { |n| n >= 0 && n <= 7}}
   end
 
-  def knight_move(final_position, position = knight.position)
-    board[position[0]][position[1]] = ' '
-    board[final_position[0]][final_position[1]] = 'N'
+  def build_path(pos, target, visited)
+    path = [target, visited.last.predecessor]
+    
+    until path.last == pos
+      path << path.last.predecessor
+    end
   end
+
 end
 
-gameboard = Board.new
-puts "#{gameboard.display_board}"
-gameboard.place_knight
-puts "#{gameboard.display_board}"
+board = Board.new
 
-gameboard.knight_move([2,2])
-puts "#{gameboard.display_board}"
-
+puts "#{board.bfs([0,1], [3,3])}"
 
--- knight.rb
+++ knight.rb
@@ -1,37 +1,14 @@
 # frozen_string_literal: true
 
 class Knight
-  attr_accessor :position, :possible_moves
+  attr_accessor :pos, :moves, :children, :predecessor
+  def initialize(pos = [0,1], predecessor = nil)
+    @pos = pos
+    @predecessor = predecessor
+    @moves = [[-2, -1], [-2, 1], [2, -1], [2, 1], [1, -2], [1, 2], [-1, -2], [-1, 2]]
 
-  def initialize(position = [0,1])
-    @position = position
-
-    @possible_moves = []
-    
   end
-
-  def build_move_list(root = @position, move_list = @possible_moves)
-
-    move_list << [root[0] + 1, root[1] + 2]
-    move_list << [root[0] + 2, root[1] + 1]
-    move_list << [root[0] - 1, root[1] - 2]
-    move_list << [root[0] - 2, root[1] - 1]
-    move_list << [root[0] + 1, root[1] - 2]
-    move_list << [root[0] + 2, root[1] - 1]
-    move_list << [root[0] - 1, root[1] + 2]
-    move_list << [root[0] - 2, root[1] + 1]
-    
-    move_list.each_with_index do |move, i|
-      move.each do |pos|
-        move_list[i] = [] if pos.negative? || pos > 7
-      end
-    end
-
-    return move_list
-  end
-
 end
-knight = Knight.new
-puts "#{knight.position}"
-knight.build_move_list([5,6])
-p knight.possible_moves
+
+# knight = Knight.new([5,7])
+
